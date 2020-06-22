bbb5653f05d11f5fa9f2bc96ac4748b8c2d41aac
Shaurya Bhandari <shauryabhandari25@gmail.com>
2020-06-22 15:44:54 +0530

    Added new project

---
+++ Follow Along Highlighter/app.js
@@ -0,0 +1,23 @@
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+document.body.append(highlight);
+
+triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
+
+function highlightLink() {
+  // alert("HI");
+  const linkCoords = this.getBoundingClientRect();
+
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  };
+
+  // console.log(linkCoords);
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
